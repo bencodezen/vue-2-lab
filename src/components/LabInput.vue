4e4e3c45bc48b04929331d736c3f6a301dffbb53
Ben Hong <ben@bencodezen.io>
2020-09-10 06:57:48 -0400

    test lab

--- src/components/LabInput.vue
+++ src/components/LabInput.vue
@@ -1,9 +1,41 @@
 <script>
-export default {};
+export default {
+  computed: {
+    inputEvents() {
+      return {
+        mouseenter: this.showTooltip,
+        input: this.updateForm
+      };
+    }
+  },
+  methods: {
+    showTooltip() {
+      console.log("Tooltip");
+    },
+    updateForm() {
+      console.log("Update");
+    },
+    testProps(item) {
+      if (item === "article") {
+        return {
+          attrs: {
+            test: "hello"
+          }
+        };
+      } else {
+        return {
+          attrs: {
+            test: "world"
+          }
+        };
+      }
+    }
+  }
+};
 </script>
 
 <template>
-  <input type="text" />
+  <input type="text" v-on="inputEvents" v-bind="testProps('article').attrs" />
 </template>
 
 <style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LabInput />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import LabInput from "@/components/LabInput.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    LabInput
   }
 };
 </script>
